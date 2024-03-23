85872b9dbc60b6fe7ca0c541e8e9221cda378578
Alexis-vs <alexis.vanstraaten@outlook.fr>
2024-03-23 23:06:55 +0100

    Reformat functions

--- R/main.R
+++ R/main.R
@@ -1,51 +1,46 @@
 # exploRail project : Functions 
 
-# Create a session on trainline.com
-session_configuration <- function(origin, destination, date){
+
+#' Create a session on trainline.com
+#'
+#' @param origin Origin city
+#' @param destination Destination city
+#' @param sess LiveHTML object with trainline session
+#'
+#' @return session
+session_configuration <- function(origin, destination, sess){
   
-  url <- paste0("https://www.thetrainline.com/book/results?journeySearchType=single&origin=urn%3Atrainline%3Ageneric%3Aloc%3A5097&destination=urn%3Atrainline%3Ageneric%3Aloc%3A4920&outwardDate=",
-                date,
-                "T00%3A00%3A26&outwardDateType=departAfter&inwardDateType=departAfter&selectedTab=train&selectExactTime=true&splitSave=true&lang=fr&transportModes[]=mixed&dpiCookieId=YWKQGT428W51RKN4OK1STWT9M&partnershipType=accommodation&partnershipSelection=true")
-  sess <- read_html_live(url)
+  # origin and destination
+  sess$type('input[id="from.search"', origin); sess$press('input[id="from.search"', "Enter")
+  Sys.sleep(1)
+  sess$type('input[id="to.search"', destination); sess$press('input[id="to.search"', "Enter")
   
-  # session check
-  if(identical(is.na(sess$html_elements("._ai52v9NaN")), logical(0))){
-    message("no session")
-  }else{
-    
-    # close pop-up window
-    Sys.sleep(2)
-    if(!is.na(sess %>% html_element(".onetrust-close-btn-handler.banner-close-button.ot-close-link"))){
-      sess$click(".onetrust-close-btn-handler.banner-close-button.ot-close-link")
-    }
-    
-    # origin and destination
-    sess$type("._ai52v9NaN", origin); sess$press("._ai52v9NaN", "Enter")
-    sess$type("._1qvaqukNaN", destination); sess$press("._1qvaqukNaN", "Enter")
-    
-    # other options like 'direct' or 'carte jeune' (young card) ?
-    
-    # validation
-    sess$click("._1o7t43lNaN")
-    
-    # click on 'plus tard' (later) button
-    Sys.sleep(1); sess$click("._aho4gji"); Sys.sleep(5); sess$click("._aho4gji")
-    Sys.sleep(5); sess$click("._aho4gji"); Sys.sleep(5); sess$click("._aho4gji")
+  # other options like 'direct' or 'carte jeune' (young card) ?
+  
+  # validation
+  sess$click("._1o7t43lNaN")
+  
+  # # click on 'plus tard' (later) button # mettre des try(sess$click si trop rapide)
+  Sys.sleep(1); sess$click("._aho4gji"); Sys.sleep(5); sess$click("._aho4gji")
+  Sys.sleep(5); sess$click("._aho4gji"); Sys.sleep(5); sess$click("._aho4gji")
+  Sys.sleep(5)
+  if(!is.na(sess %>% html_element("._aho4gji"))) sess$click("._aho4gji")
+  if(any(c(origin, destination) %in% c("Lyon", "Marseille"))){
+    # more trains for Lyon and Marseille
     Sys.sleep(5)
     if(!is.na(sess %>% html_element("._aho4gji"))) sess$click("._aho4gji")
-    if(any(c(origin, destination) %in% c("Lyon", "Marseille"))){
-      # more trains for Lyon and Marseille
-      Sys.sleep(5)
-      if(!is.na(sess %>% html_element("._aho4gji"))) sess$click("._aho4gji")
-    }
-    
-    return(sess)
-    
   }
+  
+  return(sess)
   
 }
 
-# Extract time for a trip
+
+#' Extract time for a trip
+#'
+#' @param train train
+#'
+#' @return date and time of a train
 get_time <- function(train){
   
   res <- train %>%
@@ -57,7 +52,12 @@ get_time <- function(train){
   return(res)
 }
 
-# Extract prices for a trip
+
+#' Extract prices for a trip
+#'
+#' @param train train
+#'
+#' @return prices of a train
 get_price <- function(train){
   
   if(!is.na(train %>% html_element("._iejdyhf"))){
@@ -102,7 +102,13 @@ get_price <- function(train){
 }
 
 
-# Extract type train for a trip like "inoui" or "OUIGO"
+#' Extract type train for a trip
+#' 
+#' @description Like "inoui" or "OUIGO"
+#'
+#' @param train train
+#'
+#' @return type train
 get_type_train <- function(train){
   
   type_train <- train %>%
@@ -137,49 +143,137 @@ get_informations <- function(train){
   return(res)
 }
 
-# Get information for all trains in a selected day
-get_trains <- function(origin, destination, date, local_save){
+
+wrapper <- function(origin, destination, session, local_save, save_date){
   
   time_scrap <- Sys.time()
-  session <- session_configuration(origin = origin,
-                                   destination = destination,
-                                   date = date)
   
-  if(!is.null(session)){
-    
-    # Like a filter on the chosen day in date input
-    Sys.sleep(2)
-    trains_of_the_days <- session %>%
-      html_elements("._5l6ub9") %>%
-      html_children() %>%
-      html_children() %>%
-      html_element(xpath = '//div[@role="tabpanel"]') %>%
-      html_children()
-    
-    rm(session)
-    
-    trains_informations <- lapply(trains_of_the_days, get_informations)
-    results <- do.call("rbind", trains_informations)
-    
-    results <- results %>%
-      mutate(origin = origin,
-             destination = destination,
-             time_scrap = time_scrap) %>%
-      relocate(any_of(c("origin", "destination")), .before = 1) %>%
-      mutate(across(starts_with("price"), ~replace(., is.na(.), "complet")))
-    
-    results$price_1ere[results$type_train %in% c("OUIGO", "ouigo_train_classique", "SNCF")] = "no_first_class"
-    
-    # local save
-    if(local_save == TRUE){
-      if(!dir.exists("data_temp")) dir.create("data_temp")
-      filename = paste0(paste(origin, destination, date, time_scrap, sep = "_"), ".rds")
-      results %>%
-        saveRDS(file = file.path("data_temp", filename))
-    }else{
-      return(results)
-    }
-    
+  active_session <- session_configuration(origin = origin,
+                                          destination = destination,
+                                          sess = session)
+  
+  # Like a filter on the chosen day in date input
+  Sys.sleep(2)
+  trains_of_the_days <- active_session %>%
+    html_elements("._5l6ub9") %>%
+    html_children() %>%
+    html_children() %>%
+    html_element(xpath = '//div[@role="tabpanel"]') %>%
+    html_children()
+  
+  trains_informations <- lapply(trains_of_the_days, get_informations)
+  results <- do.call("rbind", trains_informations)
+  
+  results <- results %>%
+    mutate(origin = origin,
+           destination = destination,
+           time_scrap = time_scrap) %>%
+    relocate(any_of(c("origin", "destination")), .before = 1) %>%
+    mutate(across(starts_with("price"), ~replace(., is.na(.), "complet")))
+  
+  results$price_1ere[results$type_train %in% c("OUIGO", "ouigo_train_classique", "SNCF")] = "no_first_class"
+  
+  if(local_save == TRUE){
+    if(!dir.exists("data_temp")) dir.create("data_temp")
+    scrap_hour = ifelse(as.numeric(format(Sys.time(), "%H")) < 15, "10h", "22h")
+    filename = paste0(paste(origin, destination, save_date, Sys.Date(), scrap_hour, sep = "_"), ".rds")
+    results %>%
+      saveRDS(file = file.path("data_temp", filename))
   }
+  
+  return(results)
+  
+}
+
+# date config
+maxi_wrap <- function(dates, origin, destination, session, local_save){
+  
+  date = dates %>% as.Date()
+  
+  month = format(date, format = "%m") %>% as.numeric()
+  day = format(date, format = "%d") %>% as.numeric()
+  css_date <- paste0('button[id="page.journeySearchForm.outbound.title', month, '-', day, '"') ### mettre une condition si ce n'est pas le bon mois
+  
+  # click on calendar
+  if(!is.na(session$html_elements("._uolhtnNaN"))){
+    session$click("._uolhtnNaN") 
+  }else{
+    session$click("._t0wlmwNaN") 
+  }
+  
+  Sys.sleep(0.3)
+  session$click(css_date)
+  
+  results <- pmap(list(origin, destination),
+                  .f = possibly(wrapper),
+                  session = session,
+                  local_save = local_save,
+                  save_date = dates)
+  
+  return(results)
+  
+}
+
+# Get information for all trains in a selected day
+# date au format date
+get_trains <- function(origin, destination, date, local_save = FALSE, view_current_session = FALSE){ # , session = NULL
+  
+  # checks arguments
+  if(length(origin) != length(destination)) stop("origin and destination have different length")
+  if(is.character(date[1])) stop("'date' argument is on character format. Try with date format %Y-%m-%d")
+  
+  # order date
+  
+  # open session (with the first date of 'date' vector)
+  # url <- paste0("https://www.thetrainline.com/book/results?journeySearchType=single&origin=urn%3Atrainline%3Ageneric%3Aloc%3A5097&destination=urn%3Atrainline%3Ageneric%3Aloc%3A4920&outwardDate=",
+  #               date[1],
+  #               "T00%3A00%3A26&outwardDateType=departAfter&inwardDateType=departAfter&selectedTab=train&selectExactTime=true&splitSave=true&lang=fr&transportModes[]=mixed&dpiCookieId=YWKQGT428W51RKN4OK1STWT9M&partnershipType=accommodation&partnershipSelection=true")
+  
+  url <- paste0("https://www.thetrainline.com/book/results?journeySearchType=single&origin=urn%3Atrainline%3Ageneric%3Aloc%3A4916&destination=urn%3Atrainline%3Ageneric%3Aloc%3A5097&outwardDate=",
+                date[1],
+                "T00%3A00%3A43&outwardDateType=departAfter&inwardDateType=departAfter&selectedTab=train&splitSave=true&lang=fr&transportModes[]=mixed&dpiCookieId=JAQR52NULVCEY9HK2KWEIPPUU&partnershipType=accommodation&partnershipSelection=true&selectedOutward=or57ZCqIISA%3D%3Aou4gfFrOW%2Bg%3D%3AStandard")
+  
+  sess <- read_html_live(url)
+  
+  # session check (if no bot)
+  if(identical(is.na(sess$html_elements("._ai52v9NaN")), logical(0))){
+    sess$click("._13sgtmyNaN")
+    message("no session")
+  }
+  
+  if(view_current_session == TRUE) sess$view()
+  
+  # close pop-up window
+  Sys.sleep(2)
+  if(!is.na(sess %>% html_element(".onetrust-close-btn-handler.banner-close-button.ot-close-link"))){
+    sess$click(".onetrust-close-btn-handler.banner-close-button.ot-close-link")
+  }
+  
+  # human simulation
+  Sys.sleep(1)
+  sess$scroll_by(top = sample(100:1000,1))
+  
+  # # human simulation à mettre dans la boucle ? ou montrer pate blanche avant
+  # acion_prob <- sample(c(rep(TRUE,5), rep(FALSE, 45)), 1)
+  # if(acion_prob){
+  #   Sys.sleep(3)
+  #   # scrol
+  #   Sys.sleep(3)
+  # }
+  
+  # sort dates
+  date_char = date[order(date)] %>% as.character()
+  
+  results <- lapply(X = date_char,
+                    FUN = maxi_wrap,
+                    origin = origin,
+                    destination = destination,
+                    session = sess,
+                    local_save = local_save)
+  
+  sess$session$close()
+  rm(sess)
+  
+  return(results)
   
 }
--- R/scrap.R
+++ R/scrap.R
@@ -12,57 +12,32 @@ source(file = "src/init.R")
 source(file = file.path(varglobales$dir$R, "main.R"),
        encoding = "UTF-8") # UTF-8 encoding for euro symbol in regex 
 
-local_save = TRUE
 
-# expand grid
+# Parameters for `get_trains()`
 cities_from_paris <-  c("Lyon", "Rennes", "Strasbourg", "Marseille")
-dates <- seq.Date(from = Sys.Date() + 1,
-                  to = as.Date("2024-05-22"),
+
+grid_parameters <- expand.grid(origin = "Paris",
+                               destination = cities_from_paris,
+                               stringsAsFactors = FALSE)
+
+dates <- seq.Date(from = as.Date("2024-04-17"),
+                  to = as.Date("2024-04-23"),
                   by = "day")
-
-grid_parameters_go <- expand.grid(origin = "Paris",
-                                  destination = cities_from_paris,
-                                  date = as.character(dates),
-                                  stringsAsFactors = FALSE)
-
-grid_parameters_back <- grid_parameters_go %>%
-  rename(origin = destination,
-         destination = origin) %>%
-  relocate(origin, .before = destination)
-
-
-grid_parameters <- rbind(grid_parameters_go,
-                         grid_parameters_back) %>%
-  arrange(date, origin, destination) %>%
-  mutate(local_save = TRUE)
 
 
 # get results according to grid parameters
+results <- get_trains(origin = grid_parameters$origin,
+                      destination = grid_parameters$destination,
+                      date = dates,
+                      local_save = FALSE)
 
-# try local save to save memory
-if(local_save){
-  for(i in 1:nrow(grid_parameters)){
-    try(get_trains(origin = grid_parameters$origin[i],
-                   destination = grid_parameters$destination[i],
-                   date = grid_parameters$date[i],
-                   local_save = grid_parameters$local_save[i]))
-  }
-}else{
-  results <- grid_parameters %>%
-    pmap(.f = possibly(get_trains), .progress = TRUE)
-}
-
-
-# results %>% saveRDS("save_results_to_check.rds")
-# 
-# results <- results %>%
-#   do.call(what = "rbind") %>%
-#   mutate(scrap_day = as.Date(time_scrap),
-#          scrap_hour = ifelse(as.numeric(format(time_scrap, "%H")) < 15, "10h", "22h")) %>%
-#   mutate(days_before_trip = as.Date(departure) - scrap_day,
-#          .before = "scrap_day")
-
-# # save in parquet format
-# results %>%
-#   group_by(scrap_day, scrap_hour) %>%
-#   write_dataset(path = file.path(varglobales$dir$data))
+# save in parquet format in `/data` directory
+results_df <- results %>%
+  unlist(recursive = FALSE) %>%
+  do.call(what = "rbind") %>%
+  mutate(scrap_day = as.Date(time_scrap),
+         scrap_hour = ifelse(as.numeric(format(time_scrap, "%H")) < 15, "10h", "22h")) %>%
+  mutate(days_before_trip = as.Date(departure) - scrap_day,
+         .before = "scrap_day") %>%
+  group_by(scrap_day, scrap_hour) %>%
+  write_dataset(path = file.path(varglobales$dir$data))
